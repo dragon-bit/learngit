b321be57e90b9ec5fd81df08ee061abf42a595c6
darkm0rn <45986728+darkm0rn@users.noreply.github.com>
2019-11-27 15:19:48 +0800

    添加Report的Service层
    interfReport
    ReportDo

--- shop/src/main/java/com/xupt/shop/service/interf/InterfReport.java
+++ shop/src/main/java/com/xupt/shop/service/interf/InterfReport.java
@@ -1,13 +1,13 @@
-package com.xupt.shop.dao.impl;
+package com.xupt.shop.service.interf;
 
 import com.xupt.shop.bean.Report;
 
 import java.util.List;
 
-public interface ReportDao {
-    // 添加举报
+public interface InterfReport {
 
-int addReport(int user_id, String info);
+    // 添加举报
+    int addReport(int user_id, String info);
     // 删除举报
     int deleteReport(int id);
     // 查询所有举报
